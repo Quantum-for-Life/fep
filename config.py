647fe76f3c7426eb213bd97b6ddf96d63de079ec
chem-william <zrf802@alumni.ku.dk>
2023-12-08 14:33:15 +0100

    load and generate small molecule template

--- config.py
+++ config.py
@@ -1,19 +1,27 @@
 import tomllib
 import logging
+from typing import List
 from pydantic import BaseModel, ValidationError
-
-# Define a Pydantic model for configuration
-class Settings(BaseModel):
-    temperature: float
-    pressure: float
+from pydantic import NonNegativeFloat, FilePath
 
 
-def load_config(file_path):
+
+class SystemSettings(BaseModel):
+    file_name: FilePath
+    smiles: str
+    temperature: NonNegativeFloat
+    pressure: NonNegativeFloat
+    sterics_lambdas: List[NonNegativeFloat]
+    electrostatics_lambdas: List[NonNegativeFloat]
+
+
+def load_config(file_path) -> SystemSettings:
     """Load and validate configuration from a TOML file using Pydantic."""
     try:
         with open(file_path, "rb") as file:
             config_data = tomllib.load(file)
-        return Settings(**config_data["settings"])
+            if "system-settings" in config_data:
+                return SystemSettings(**config_data["system-settings"])
     except ValidationError as e:
         logging.error("Configuration validation error: %s", e)
         raise
--- main.py
+++ main.py
@@ -1,9 +1,46 @@
 import sys
+from pathlib import PosixPath
 import argparse
 import logging
 import config
+from config import SystemSettings
 
 # from pymbar import MBAR, timeseries
+import mdtraj
+from openff.toolkit import Molecule
+from openmm.app import (
+    ForceField,
+    HBonds,
+    Modeller,
+    PDBFile,
+    PDBReporter,
+    PME,
+    Simulation,
+    StateDataReporter,
+)
+from openmmforcefields.generators import GAFFTemplateGenerator
+from openmmtools import alchemy
+import openmm
+from openmm import (
+    CustomNonbondedForce,
+    LangevinIntegrator,
+    LangevinMiddleIntegrator,
+    MonteCarloBarostat,
+    NonbondedForce,
+    XmlSerializer,
+)
+from openmm.unit import (
+    AVOGADRO_CONSTANT_NA,
+    BOLTZMANN_CONSTANT_kB,
+    amu,
+    angstrom,
+    atmospheres,
+    femtoseconds,
+    kelvin,
+    kilocalories_per_mole,
+    nanometer,
+    picoseconds,
+)
 
 
 def setup_logging():
@@ -11,6 +48,50 @@ def setup_logging():
     logging.basicConfig(
         level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s"
     )
+
+
+def create_alchemical_system(
+    molecule_file: openmm.app.pdbfile.PDBFile,
+    forcefield: openmm.app.ForceField,
+    SystemSettings: config.SystemSettings,
+) -> openmm.System:
+    modeller = Modeller(molecule_file.topology, molecule_file.positions)
+    modeller.addSolvent(forcefield, padding=1.1 * nanometer)
+
+    system = forcefield.createSystem(
+        modeller.topology,
+        nonbondedMethod=PME,
+        nonbondedCutoff=1.0 * nanometer,
+        switchDistance=0.9 * nanometer,
+        constraints=HBonds,
+        rigidWater=True,
+        hydrogenMass=1.5 * amu,
+    )
+
+    factory = alchemy.AbsoluteAlchemicalFactory()
+
+    small_molecule_atoms = mdtraj.Topology.from_openmm(modeller.topology).select(
+        "resname UNL"
+    )
+    alchemical_region = alchemy.AlchemicalRegion(alchemical_atoms=small_molecule_atoms)
+    alchemical_system = factory.create_alchemical_system(system, alchemical_region)
+
+    return alchemical_system
+
+
+def load_small_molecule(
+    file_name: PosixPath, smiles: str, forcefield: openmm.app.ForceField
+) -> None:
+    if file_name.suffix == ".pdb":
+        loaded_molecule = PDBFile(f"{file_name}")
+    else:
+        raise ValueError(f"file format not implemented: {file_name}")
+
+    molecule = Molecule.from_smiles(f"{smiles}")
+    gaff = GAFFTemplateGenerator(molecules=molecule)
+    forcefield.registerTemplateGenerator(gaff.generator)
+
+    return loaded_molecule
 
 
 def main():
@@ -27,11 +108,12 @@ def main():
     args = parser.parse_args()
 
     cfg = config.load_config(args.config)
-
     logging.info(f"Configuration loaded and validated: {cfg}")
 
-    logging.info(f"Temperature value: {cfg.temperature}")
-    logging.info(f"Temperature value: {cfg.pressure}")
+    forcefield = ForceField("amber/protein.ff14SB.xml", "amber/tip3p_standard.xml")
+    loaded_molecule = load_small_molecule(cfg.file_name, cfg.smiles, forcefield)
+
+    create_alchemical_system(loaded_molecule, forcefield, cfg)
 
 
 if __name__ == "__main__":
