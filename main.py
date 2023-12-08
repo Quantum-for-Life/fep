e3e591f29642d64fec1824193e9f02468736abc7
chem-william <zrf802@alumni.ku.dk>
2023-12-08 10:59:31 +0100

    run lint

--- main.py
+++ main.py
@@ -3,31 +3,41 @@ import argparse
 import tomllib
 import logging
 
+
 def load_config(file_path):
     """Load configuration from a TOML file."""
-    with open(file_path, 'r') as file:
+    with open(file_path, "r") as file:
         config = tomllib.load(file)
     return config
 
+
 def setup_logging():
     """Setup basic logging configuration."""
-    logging.basicConfig(level=logging.INFO,
-                        format='%(asctime)s - %(levelname)s - %(message)s')
+    logging.basicConfig(
+        level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s"
+    )
+
 
 def main():
     setup_logging()
 
-    parser = argparse.ArgumentParser(description='CLI App with TOML Config')
-    parser.add_argument('-c', '--config', type=str, default='config.toml',
-                        help='Path to the configuration file.')
+    parser = argparse.ArgumentParser(description="CLI App with TOML Config")
+    parser.add_argument(
+        "-c",
+        "--config",
+        type=str,
+        default="config.toml",
+        help="Path to the configuration file.",
+    )
     args = parser.parse_args()
 
     config = load_config(args.config)
 
     logging.info("Configuration loaded:", config)
-    
-    logging.info("Temperature value:", config['settings']['temperature'])
-    logging.info("Pressure value:", config['settings']['pressure'])
 
-if __name__ == '__main__':
+    logging.info("Temperature value:", config["settings"]["temperature"])
+    logging.info("Pressure value:", config["settings"]["pressure"])
+
+
+if __name__ == "__main__":
     sys.exit(main())
